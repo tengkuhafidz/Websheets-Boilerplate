4eb07bdd40eb6d238e9b425480e28a503e3912aa
Tengku Hafidz <muhammad_hafidzuddin@tech.gov.sg>
2021-03-27 12:34:24 +0800

    push footer to bottom of page

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -4,9 +4,9 @@ import SEO from './seo'
 export default function Layout({children}) {
 	return (
 		<div className="text-gray-900">
-			<div className="container mx-auto max-w-5xl py-4 px-4">
+			<div className="container mx-auto max-w-5xl py-4 px-4 flex flex-col min-h-screen">
 				<SEO />
-				<main>{children}</main>
+				<main className="flex-grow">{children}</main>
 				<Footer />
 			</div>
 		</div>
